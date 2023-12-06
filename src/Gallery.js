019e55fd382d9ff3045c5226ee66ebc7dbb30284
alfiexbaker <williamalfredbaker@gmail.com>
2023-12-06 18:44:15 +0000

    k
    n

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "./slick-carousel/slick/slick.css";
+import "./slick-carousel/slick/slick-theme.css";
 import './Gallery.css';
 
 const Gallery = ({ source }) => {
